6f67788dcc6bae203d5e19e225c4de00a5cb70d5
Felipe Acelino <felipeacelino@hotmail.com>
2018-07-16 23:47:33 -0300

    Header and footer

---
+++ wp-content/themes/bikcraft/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title><?php bloginfo('name'); ?></title>
+		<meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
+
+		<meta property="og:type" content="website"/>
+		<meta property="og:title" content="Bikcraft - Bicicletas Personalizadas"/>
+		<meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte."/>
+		<meta property="og:url" content="http://bikcraft.com"/>
+		<meta property="og:image" content="http://bikcraft.com/img/og-image.png"/>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="shortcut icon" href="favicon.ico">
+
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+        <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/libs/modernizr.custom.45655.js"></script>
+        
+        <!-- Início WordPress Header -->
+        <?php wp_head(); ?>
+        <!-- Fim WordPress Header -->
+	</head>
+	<body>
+
+		<header class="header">
+			<div class="container">
+				<a href="index.html" class="grid-4">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/bikcraft.png" alt="Bikcraft">
+				</a>
+				<nav class="grid-12 header_menu">
+					<ul>
+						<li><a href="curso-wordpress/sobre">Sobre</a></li>
+						<li><a href="curso-wordpress/produtos.html">Produtos</a></li>
+						<li><a href="curso-wordpress/portfolio.html">Portfólio</a></li>
+						<li><a href="curso-wordpress/contato.html">Contato</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
--- wp-content/themes/bikcraft/index.php
+++ wp-content/themes/bikcraft/index.php
@@ -1,40 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-	<head>
-		<meta charset="utf-8">
-		<title>Bikcraft - Bicicletas Personalizadas</title>
-		<meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
-
-		<meta property="og:type" content="website"/>
-		<meta property="og:title" content="Bikcraft - Bicicletas Personalizadas"/>
-		<meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte."/>
-		<meta property="og:url" content="http://bikcraft.com"/>
-		<meta property="og:image" content="http://bikcraft.com/img/og-image.png"/>
-
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<link rel="shortcut icon" href="favicon.ico">
-
-		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-		<script src="js/libs/modernizr.custom.45655.js"></script>
-	</head>
-	<body>
-
-		<header class="header">
-			<div class="container">
-				<a href="index.html" class="grid-4">
-					<img src="img/bikcraft.png" alt="Bikcraft">
-				</a>
-				<nav class="grid-12 header_menu">
-					<ul>
-						<li><a href="sobre.html">Sobre</a></li>
-						<li><a href="produtos.html">Produtos</a></li>
-						<li><a href="portfolio.html">Portfólio</a></li>
-						<li><a href="contato.html">Contato</a></li>
-					</ul>
-				</nav>
-			</div>
-		</header>
+<?php get_header(); ?>
 
 		<section class="introducao">
 			<div class="container">
@@ -43,7 +7,7 @@
 					<p>“não tenha nada em sua casa que você não considere útil ou acredita ser bonito”</p>
 					<cite>WILLIAM MORRIS</cite>
 				</blockquote>
-				<a href="produtos.html" class="btn">Orçamento</a>
+				<a href="curso-wordpress/produtos" class="btn">Orçamento</a>
 			</div>
 		</section>
 		
@@ -53,7 +17,7 @@
 
 				<li class="grid-1-3">
 					<div class="produtos_icone">
-						<img src="img/produtos/passeio.png" alt="Bikcraft Passeio">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/produtos/passeio.png" alt="Bikcraft Passeio">
 					</div>
 					<h3>Passeio</h3>
 					<p>Muito melhor do que passear pela orla a vidros fechados.</p>
@@ -61,7 +25,7 @@
 
 				<li class="grid-1-3">
 					<div class="produtos_icone">
-						<img src="img/produtos/esporte.png" alt="Bikcraft Esporte">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/produtos/esporte.png" alt="Bikcraft Esporte">
 					</div>
 					<h3>Esporte</h3>
 					<p>Mais rápida do que Forrest Gump, ninguém vai pegar você.</p>
@@ -69,7 +33,7 @@
 
 				<li class="grid-1-3">
 					<div class="produtos_icone">
-						<img src="img/produtos/retro.png" alt="Bikcraft Retrô">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/produtos/retro.png" alt="Bikcraft Retrô">
 					</div>
 					<h3>Retrô</h3>
 					<p>O passado volta para lembrarmos o que devemos fazer no futuro.</p>
@@ -79,7 +43,7 @@
 
 			<div class="call">
 				<p>clique aqui e veja os detalhes dos produtos</p>
-				<a href="produtos.html" class="btn btn-preto">Produtos</a>
+				<a href="curso-wordpress/produtos" class="btn btn-preto">Produtos</a>
 			</div>
 
 		</section>
@@ -89,20 +53,20 @@
 			<div class="container">
 				<h2 class="subtitulo">Portfólio</h2>
 				<div class="portfolio_lista">
-					<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-					<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-					<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
+					<div class="grid-8"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
+					<div class="grid-8"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
+					<div class="grid-16"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
 				</div>
 				<div class="call">
 					<p>conheça mais o nosso portfólio</p>
-					<a href="portfolio.html" class="btn">Portfólio</a>
+					<a href="curso-wordpress/portfolio" class="btn">Portfólio</a>
 				</div>
 			</div>
 		</section>
 
 		<section class="qualidade container">
 			<h2 class="subtitulo">Qualidade</h2>
-			<img src="img/bikcraft-qualidade.png" alt="Bikcraft">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/bikcraft-qualidade.png" alt="Bikcraft">
 			<ul class="qualidade_lista">
 				<li class="grid-1-3">
 					<h3>Durabilidade</h3>
@@ -119,70 +83,8 @@
 			</ul>
 			<div class="call">
 				<p>conheça mais a nossa história</p>
-				<a href="sobre.html" class="btn btn-preto">Sobre</a>
+				<a href="curso-wordpress/sobre" class="btn btn-preto">Sobre</a>
 			</div>
 		</section>
 
-		<div class="quebra">
-			<blockquote class="quote-externo container">
-				<p>“o verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana.”</p>
-				<cite>WILLIAM MORRIS</cite>
-			</blockquote>
-		</div>
-
-		<footer>
-			<div class="footer">
-				<div class="container">
-
-					<div class="grid-8 footer_historia">
-						<h3>Nossa História</h3>
-						<p>Quando iniciamos a Bikcraft queriamos apenas um produto que adoraríamos utilizar. Eramos apaixonados por pedalar e também por fazer as coisas com as nossas próprias mãos. Assim surgiu um sonho na garagem da nossa casa.</p>
-					</div>
-
-					<div class="grid-4 footer_contato">
-						<h3>Contato</h3>
-						<ul>
-							<li>- 21 9999-9999</li>
-							<li>- contato@bikcraft.com</li>
-							<li>- Botafago - RJ</li>
-						</ul>
-					</div>
-
-					<div class="grid-4 footer_redes">
-						<h3>Contato</h3>
-						<ul>
-							<li><a href="http://facebook.com" target="_blank"><img src="img/redes-sociais/facebook.png" alt="Facebook Bikcraft"></a></li>
-							<li><a href="http://instagram.com" target="_blank"><img src="img/redes-sociais/instagram.png" alt="Instagram Bikcraft"></a></li>
-							<li><a href="http://twitter.com" target="_blank"><img src="img/redes-sociais/twitter.png" alt="Twitter Bikcraft"></a></li>
-						</ul>
-					</div>
-
-				</div>
-			</div>
-
-			<div class="copy">
-				<div class="container">
-					<p class="grid-16">Bikcraft 2015 - Alguns direitos reservados.</p>
-				</div>
-			</div>
-		</footer>
-
-	<!-- JavaScript -->
-	<script src="js/libs/jquery-1.11.2.min.js"></script>
-	<script src="js/plugins.js"></script>
-	<script src="js/main.js"></script>
-	<!-- JavaScript -->
-
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-60088262-1', 'auto');
-	  ga('send', 'pageview');
-
-	</script>
-
-	</body>
-</html>
+<?php get_footer(); ?>
