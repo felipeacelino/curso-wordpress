4bae3abee9815b81cb4f1d7a70ff849de6cc6b13
Felipe Acelino <felipeacelino@hotmail.com>
2018-07-21 12:41:41 -0300

    Portfólio

--- wp-content/themes/bikcraft/page-portfolio.php
+++ wp-content/themes/bikcraft/page-portfolio.php
@@ -10,26 +10,14 @@
 
 <section class="container animar-interno">
 	<ul class="rslides">
+		<?php if(have_rows('depoimentos')): while(have_rows('depoimentos')) : the_row(); ?>
 		<li>
 			<blockquote class="quote_clientes">
-				<p>“Pedalar sempre foi a minha paixão, o que o pessoal da Bikcraft fez foi intensificar o meu amor por esta atividade. Recomendo à todos que amo.”</p>
-				<cite>Barbara Moss</cite>
+				<p><?php the_sub_field('depoimento'); ?></p>
+				<cite><?php the_sub_field('nome'); ?></cite>
 			</blockquote>
 		</li>
-
-		<li>
-			<blockquote class="quote_clientes">
-				<p>“Nada melhor do que dar um rolê com a minha Bikcraft na orla. Essa é a minha companheira mais fiel, nunca me traiu e está sempre a minha disposição.”</p>
-				<cite>Jhony Rato</cite>
-			</blockquote>
-		</li>
-
-		<li>
-			<blockquote class="quote_clientes">
-				<p>“Aqueles que ainda não possuem uma Bikcraft, não sabem o que estão perdendo. A precisão é absurda e a velocidade transcendental. Nunca vida nada igual.”</p>
-				<cite>Bernardo</cite>
-			</blockquote>
-		</li>
+		<?php endwhile; else : endif; ?>
 	</ul>
 </section>
 
